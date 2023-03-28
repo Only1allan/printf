e26423044ae04bd1cbc461a88ff94e7454c343ca
GraceBuri <grcbuuri@gmail.com>
2023-03-28 11:25:20 -0700

    Printf.c and supporting functions added

---
+++ _puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - a function that returns a string
+ * @string: parameter for the _puts function
+ * Return: string
+ */
+int _puts(char *string)
+{
+        int string_position;
+
+        for (string_position = 0; string[string_position]; string_position++)
+        {
+                _putchar(string[string_position]);
+        }
+        return (string_position);
+}
