6b316a95089ef26589b4e2b523a0a20e54f85e0f
Only1allan <kariukiallan850@gmail.com>
2023-03-28 11:46:00 -0700

    added header file and fixed betty

--- printf.c
+++ printf.c
@@ -1,47 +1,43 @@
 #include "main.h"
 #include <stdarg.h>
 /**
- * _printf - a function that replicated the standard printf functions
- * @format: an argument for the print f function
- * Return: 0 always a success
- */
+* _printf - a function that replicated the standard printf functions
+* @format: an argument for the print f function
+* Return: 0 always a success
+*/
 int _printf(const char *format, ...)
 {
-        va_list parameters;
-        va_start(parameters, format);
+	va_list parameters;
 
-        while (*format)
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        if (*format == 'c')
-                        {
-                                _putchar(va_arg(parameters, int));
-                        }
-                        else if (*format == 's')
-                        {
-                                _puts(va_arg(parameters, char *));
-                        }
-                        else if (*format == '%')
-                        {
-                                _putchar(va_arg(parameters, int));
-                        }
-                        else if (*format == 'd'|| *format == 'i')
-                        {
-                                _putsdigit(va_arg(parameters, int));
-                        }
-                        else
-                        {
-                                _putchar(*format);
-                        }
-                }
-                else
-                {
-                        _putchar(*format);
-                }
-                format++;
-        }
-        va_end(parameters);
-        return (0);
+	va_start(parameters, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				_putchar(va_arg(parameters, int));
+			}
+			else if (*format == 's')
+			{
+				_puts(va_arg(parameters, char *));
+			}
+			else if (*format == '%')
+			{
+				_putchar(va_arg(parameters, int));
+			}
+			else
+			{
+				_putchar(*format);
+			}
+		}
+		else
+		{
+			_putchar(*format);
+		}
+		format++;
+	}
+	va_end(parameters);
+	return (0);
 }
